5b1003093ef15c6c388ecfa4dee60e12b2956cf3
innocent <innocent.wasia@studentmoringaschool.com>
2024-11-28 00:14:41 +0300

    js-2-looping-code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Code your solution
+function writeCards(names, event) {
+    const messages = []; // an empty array
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages; // Return the array
+  }
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number); // Log the current number
+    number--; // Decrement  number
+  }
+}
+module.exports = { writeCards, countDown };
+
